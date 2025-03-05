bcc22f9171a1f2866a257322824dfcffea20602e
Federico Spagnolo <federico.spagnolo@unibas.ch>
2025-03-05 16:29:35 +0100

    Delete train_AB_FS directory

--- train_AB_FS/test_2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#usage: sh test_2.sh T1 QSM Mask
-
-python test_rimnet.py --modalities $1 $2 $3 --folder 1 --sub_folder 3
-python test_rimnet.py --modalities $1 $2 $3 --folder 2 --sub_folder 2
-python test_rimnet.py --modalities $1 $2 $3 --folder 3 --sub_folder 3
-python test_rimnet.py --modalities $1 $2 $3 --folder 4 --sub_folder 3
-python test_rimnet.py --modalities $1 $2 $3 --folder 5 --sub_folder 3
